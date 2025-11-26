8642c116b4c8d1375c6da645adc760bdc8b960fa
Jayantasaha01 <144397397+Jayantasaha01@users.noreply.github.com>
2025-11-26 18:47:29 +0000

    change

--- firewall.py
+++ firewall.py
@@ -3,7 +3,7 @@ import json, sys
 
 RULES = [
     # Block traffic from this IP range
-    {"action": "DROP", "src":"10.0.0.0/8"},
+    {"action": "DROP", "src":"10.0.0.0/24"},
     # Allow traffic from this IP range
     {"action": "ALLOW", "src": "203.0.113.0/24"}
 ]
